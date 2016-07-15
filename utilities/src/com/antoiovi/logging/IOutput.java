dd31f5ac8b0672b9d23400fb0b84bf12db010e00
antoiovi <antoiovi@antoiovi.com>
2016-07-15 19:52:28 +0200

    aggiunto OutputStandard  modificato IOutput

--- utilities/src/com/antoiovi/logging/IOutput.java
+++ utilities/src/com/antoiovi/logging/IOutput.java
@@ -18,11 +18,9 @@ public interface IOutput {
 	public void setMessage();
 	public void appendMessage();
 	
-	public class Level{
-		public static final String ERROR="ERROR";
-		public static final String WARNING="WARNING";
-		public static final String INFO="INFO";
-		public static final String DEBUG="DEBUG";
-		public static final String TRACE="TRACE";
+	public enum Level{
+		     NOOUT ,  ERROR ,WARNING,INFO,DEBUG,TRACE
+		 
 	}
+ 
 }
