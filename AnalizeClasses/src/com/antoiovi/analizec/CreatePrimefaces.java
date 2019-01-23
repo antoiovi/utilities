e02a3b1f4191aafe97baaf06781762339257d93b
antoiovi <antoiovi@antoiovi.com>
2019-01-23 18:30:56 +0100

    Completato creazione tabella xhtml e iniziato controllo annotations

--- AnalizeClasses/src/com/antoiovi/analizec/CreatePrimefaces.java
+++ AnalizeClasses/src/com/antoiovi/analizec/CreatePrimefaces.java
@@ -3,22 +3,45 @@ package com.antoiovi.analizec;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.net.URLClassLoader;
 import java.util.List;
+import javax.validation.constraints.Size;
 
 public class CreatePrimefaces {
 
 	private Class selectedClass;
+	private String className;
 
 	Field field[];
+	String managedBeanClassName;
+	String managedBeanName;
+	String strListAll;
+	String var;
 
 	public CreatePrimefaces(Class selectedClass) {
 		this.selectedClass = selectedClass;
 		field = selectedClass.getDeclaredFields();
+		String s=String.format("Name Class %s \n Canonicalname %s", selectedClass.getName(),selectedClass.getCanonicalName());
+log(s);
+String[] tokens = selectedClass.getName().split("\\.");
 
+   className=tokens[tokens.length-1];
+   managedBeanClassName=className+"Bean";
+   log(className);
+   char c[] = className.toCharArray();
+   c[0] = Character.toLowerCase(c[0]);
+   var = new String(c);
+   managedBeanName=var+"Bean";
+   log(managedBeanName);
+   log(var);
+   strListAll="all"+className;
+   log(strListAll);
+   
+   
 	}
 
 	public void executeTaskOnSelectedClass() {
@@ -58,30 +81,43 @@ public class CreatePrimefaces {
 					+ "		xmlns:ui=\"http://java.sun.com/jsf/facelets\"\n"
 					+ "		xmlns:h=\"http://java.sun.com/jsf/html\"\n"
 					+ "		xmlns:f=\"http://java.sun.com/jsf/core\"\n" + "		xmlns:p=\"http://primefaces.org/ui>\n",
-			"<ui:define name=\"content\">\n", "" };
+			" <ui:define name=\"content\">\n", "" };
 
 	public void printHeader(PrintWriter writer) {
 		for (int x = 0; x < header.length; x++) {
 			writer.print(header[x]);
 		}
 	}
+	
+	
 
 	public void createTable() {
+		String value;
 		PrintWriter writer;
 		try {
 			writer = new PrintWriter("Table.xhtml", "UTF-8");
 			printHeader(writer);
+			
+			String t1=String.format("\t"+"<p:dataTable var=\"%s\" value=\"#{%s.%s}\"> ",var,managedBeanName,strListAll);
+					
+			
+			writer.println(t1);
+			
 			for (int x = 0; x < field.length; x++) {
 				Field f = field[x];
-				String s = String.format("\tField name : %s Type %s", f.getName(), f.getType().toString());
-				log(s);
-				String s1 = String.format("<p:column headerText=\"%s width=\"100\">", f.getName());
-				String s2 = String.format("<h:outputText value=\"#{%s}\"/>", f.getName());
+				int width=findFieldWidth(f);
+				String s = String.format("\t"+"\tField name : %s Type %s", f.getName(), f.getType().toString());
+			//	log(s);
+				String s1 = String.format("\t\t"+"<p:column headerText=\"%s\" width=\"%d\">", f.getName(),width);
+				String s2 = String.format("\t\t\t"+"<h:outputText value=\"#{%s.%s}\"/>", var,f.getName());
 				writer.println(s1);
 				writer.println(s2);
-				writer.println("</p:column>");
+				writer.println("\t\t"+"</p:column>");
 
 			}
+			writer.println("\t"+"</p:dataTable>");
+			writer.println(" </ui:define>\n" + 
+					"</ui:composition>");
 
 			writer.close();
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
@@ -89,6 +125,22 @@ public class CreatePrimefaces {
 			e.printStackTrace();
 		}
 
+	}
+	
+	int findFieldWidth(Field f) {
+		int val=25;
+		Annotation a[]= f.getDeclaredAnnotations();
+		Size sizeAnn = f.getAnnotation(Size.class);
+		   if (sizeAnn != null) {
+			   log("MAX  "+sizeAnn.max());
+		    return sizeAnn.max()==0?val:sizeAnn.max();
+			   //System.out.println(""+sizeAnn.toString());
+		   }
+		   else {
+			   log("max =null");
+			   return val;
+		   }
+		       
 	}
 
 	protected static void log(String s) {
